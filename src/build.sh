32c9c95a6881188cba38c66b3b9f2b17cd4da28b
Ivan Dasilva <ralntdir@gmail.com>
2015-12-04 00:00:15 +0100

    Creating the structure for the RT

--- src/build.sh
+++ src/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ rt.cpp -lSDL2 -I. -o ../build/rt
+g++ rt.cpp -std=c++11 -lSDL2 -I. -o ../build/rt
--- src/rt.cpp
+++ src/rt.cpp
@@ -1,10 +1,27 @@
 #include <SDL2/SDL.h>
 #include <iostream>
+#include <vector>
+
+#include <float.h>
+
 
 using namespace std;
 
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
+
+#define WIDTH 100
+#define HEIGHT 100
+
+struct v3
+{
+  float x;
+  float y;
+  float z;
+};
+
+typedef int* Sphere;
+typedef int Ray;
 
 bool init()
 {
@@ -17,7 +34,7 @@ bool init()
   }
   else
   {
-    window = SDL_CreateWindow("Devember", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+    window = SDL_CreateWindow("Devember", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
 
     if (window == NULL)
     {
@@ -54,6 +71,70 @@ void close()
   SDL_Quit();
 }
 
+float myDistance(v3 point1, v3 point2)
+{
+  return 0.0f;
+}
+
+bool intersect(Sphere sphere, Ray ray, v3 *hitPoint)
+{
+  // first version for an intersect method
+  // we just need the hit point for the intersection
+  return false;
+}
+
+void render()
+{
+  // Creates an image using the ray tracing method.
+  // The first prototype will have only spheres and flat shading.
+  // Then I will add diffuse shading (lights) and after that
+  // shadows.
+  vector<Sphere> scene;
+  v3 eyePosition;
+
+  v3 image[WIDTH][HEIGHT] = {};
+
+  // prepare the scene
+
+  // trace rays
+  for (int i = 0; i < WIDTH; i++)
+  {
+    for (int j = 0; j < HEIGHT; j++)
+    {
+      // create the ray based in eye position, i and j
+      Ray ray;
+      v3 hitPoint;
+      float minDistance = FLT_MAX;
+      
+      Sphere closerObject = NULL;
+
+      for (int k = 0; k < scene.size(); k++)
+      {
+        if (intersect(scene[k], ray, &hitPoint))
+        {
+          // Calculate the distance between hitPoint and eye
+          // If it is less than the previous one, this is 
+          // the closer object, and we have to store it in order
+          // to take its color as the color for this pixel
+          float dist = myDistance(eyePosition, hitPoint);
+          
+          if (dist < minDistance)
+          {
+            closerObject = scene[k];
+            minDistance = dist;
+          }
+        } 
+      }
+
+      // For the moment is going to be always NULL
+      if (closerObject == NULL)
+      {
+        image[i][j] = {255.0, 0.0, 0.0};
+      }
+    }
+  }
+}
+
 int main(int argc, char* argv[])
 {
   if (!init())
@@ -72,7 +153,8 @@ int main(int argc, char* argv[])
       {
         quit = true;
       }
-    }    
+    }
+    render();
   }
 
   close();
