91dea02ad38676f509603617ad85ac10b82e2845
Ivan Dasilva <ralntdir@gmail.com>
2015-12-09 01:40:55 +0100

    Changing the computation of the primary ray. Flat shaded image obtained! :)

--- src/rt.cpp
+++ src/rt.cpp
@@ -10,8 +10,8 @@
 
 using namespace std;
 
-#define WIDTH 35
-#define HEIGHT 35
+#define WIDTH 500
+#define HEIGHT 500
 
 struct v3
 {
@@ -160,13 +160,17 @@ bool intersect(Sphere sphere, Ray ray, v3 *hitPoint)
   // we just need the hit point for the intersection
   bool success;
   float a = dotProduct(ray.direction, ray.direction);
+  //cout << a << endl;
   float b = 2 * dotProduct(ray.direction, 
                            subtraction(ray.origin, sphere.center)); 
+  //cout << b << endl;
   float c = dotProduct(subtraction(ray.origin, sphere.center),
                        subtraction(ray.origin, sphere.center)) - 
             sphere.radius * sphere.radius;
 
+  //cout << c << endl;
   float discriminant = b * b - 4 * a * c;
+  //cout << discriminant << endl;
   // If the discriminant is less than cero, we have complex roots
   if (discriminant < 0)
   {
@@ -213,25 +217,7 @@ bool intersect(Sphere sphere, Ray ray, v3 *hitPoint)
     }
   }
 
-  success = false;
   return success;
-}
-
-v3 calculateDirection(v3 origin, float x, float y)
-{
-  v3 result = {};
-
-  // We add 0.5 as we want the direction to the
-  // center of the pixel. We make z=1 because
-  // thats where the screen is located?
-  // TODO(ivan): check screen position
-  v3 pixelCoords = {x + 0.5f, y + 0.5f, 1.0f};
-
-  result = subtraction(pixelCoords, origin);
-  //printV3(result);
-  result = normalize(result);
-
-  return result;
 }
 
 void render()
@@ -243,6 +229,8 @@ void render()
 
   // TODO(ivan): I need to define the camera and the plane
   // I'm projecting the scene on!!!
+  // For the moment, the camera is at (0, 0, 0) and pointing 
+  // to +Z and the plane is an XY plane at Z=1;
 
   // TODO(ivan): Decide the coordinate system. Right now I'm 
   // working with a left handed cs, but maybe I should change to
@@ -265,25 +253,31 @@ void render()
   // prepare the scene
   Sphere testSphere = {};
   testSphere.center = {0.0f, 0.0f, 4.0f};
-  testSphere.radius = 30.0f;
+  testSphere.radius = 3.0f;
   testSphere.color = {255.0f, 255.0f, 0.0f};
   scene.push_back(testSphere);
 
+  // In degrees
+  float fov = 100.0f;
+  float aspectRatio = (float)WIDTH / (float)HEIGHT;
+
   // trace rays
-  for (int i = 0; i < WIDTH; i++)
+  for (int j = 0; j < HEIGHT; j++)
   {
-    for (int j = 0; j < HEIGHT; j++)
+    for (int i = 0; i < WIDTH; i++)
     {
       // create the ray based in eye position, i and j
       Ray ray;
       ray.origin = eyePosition;
-      // I need to calculate the direction of the ray I'm
-      // casting based on its origin, i and j?? Check the 
-      // theory, as I am a bit lost in this part.
-      //ray.direction = ???;
-      ray.direction = calculateDirection(ray.origin, i - WIDTH/2, j - HEIGHT/2);
-      printV3(ray.direction);
+      float pX = (2 * ((i + 0.5) / WIDTH) - 1) * tan(fov / 2 * M_PI / 180) * aspectRatio;
+      float pY = (1 - 2 * ((j + 0.5) / HEIGHT)) * tan(fov / 2 * M_PI / 180);
+      // As the origin is (0, 0, 0), I don't need to do the subtraction
+      ray.direction = {pX, pY, 1.0f};
+      ray.direction = normalize(ray.direction);
+      //printV3(ray.direction);
+
       v3 hitPoint;
+
       float minDistance = FLT_MAX;
       
       Sphere *closerObject = NULL;
@@ -292,7 +286,7 @@ void render()
       {
         if (intersect(scene[k], ray, &hitPoint))
         {
-          cout << "FOO" << endl;
+          //cout << "FOO" << endl;
           // Calculate the distance between hitPoint and eye
           // If it is less than the previous one, this is 
           // the closer object, and we have to store it in order
@@ -305,12 +299,17 @@ void render()
             minDistance = dist;
           }
         } 
+        //exit(1);
       }
 
       // For the moment is going to be always NULL
       if (closerObject == NULL)
       {
         image[i][j] = color;
+      }
+      else
+      {
+        image[i][j] = closerObject->color;
       }
     }
   }
